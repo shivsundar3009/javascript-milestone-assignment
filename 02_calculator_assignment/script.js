a8e808079c565af0d6bd1f9ce987653fab5b4125
shivsundar3009 <shivsundar3009@gmail.com>
2023-12-27 00:08:34 +0530

    switch case working change has to be made

--- 02_calculator_assignment/script.js
+++ 02_calculator_assignment/script.js
@@ -10,6 +10,25 @@ const btn = document.getElementById('btn')
 btn.addEventListener('click',()=>{
     console.log(firstValue.value);
     console.log(secondValue.value);
+    
+    switch(operatorValue.value){
+        case "+":
+            console.log("addition");
+            break;
+        case "-":
+            console.log('minus');
+            break;
+        case "*":
+            console.log('multiply');
+            break;
+        case "/":
+            console.log('division');
+            break;
+        
+        default:
+            console.log('error');  
+            break;  
+    }
     if(operatorValue.value === "+"){
         console.log(Number(firstValue.value) + Number(secondValue.value));
     }
