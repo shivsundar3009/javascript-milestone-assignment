14b7149dd708dbcdb00c91023dc3c46c9e6fdac1
shivsundar3009 <shivsundar3009@gmail.com>
2023-12-28 22:16:51 +0530

    02_calculator_assignment completed

--- 02_calculator_assignment/script.js
+++ 02_calculator_assignment/script.js
@@ -6,28 +6,55 @@ const operatorValue = document.getElementById('operatorValue')
 
 const btn = document.getElementById('btn')
 
+const clearBtn = document.getElementById('clear')
+
 const result = document.getElementById('result')
 
 btn.addEventListener('click',()=>{
+
+    result.style.display = 'block'
+
+    let resultVal; 
+
+    result.style.backgroundColor = 'green';
+
+    result.style.color = 'white'
+
+    result.style.padding = '20px'
     
-    switch(operatorValue.value){
+    switch(operatorValue.value.toLowerCase()){
         case "+":
-            result.innerText = "Result is : "; Number(firstValue.value) + Number(secondValue.value);
+            console.log(Number(firstValue.value) + Number(secondValue.value));
+            resultVal = Number(firstValue.value) + Number(secondValue.value)
+            result.innerText = `Result is : ${resultVal}`
             break;
         case "-":
-            result.innerText = "Result is : "; Number(firstValue.value) - Number(secondValue.value);
+            resultVal = Number(firstValue.value) - Number(secondValue.value)
+            result.innerText = `Result is : ${resultVal}`
             break;
         case "*":
-            result.innerText = "Result is : "; Number(firstValue.value) * Number(secondValue.value);
+            resultVal = Number(firstValue.value) * Number(secondValue.value)
+            result.innerText = `Result is : ${resultVal}`
             break;
         case "/":
-            result.innerText = "Result is : "; Number(firstValue.value) / Number(secondValue.value);
+            resultVal = Number(firstValue.value) / Number(secondValue.value)
+            result.innerText = `Result is : ${resultVal}`
             break;
         
         default:
-            console.log('Please enter correct operator');  
+            result.innerText = 'Please enter correct operator'
             break;  
     }
 
 })
+
+clearBtn.addEventListener('click', ()=>{
+    firstValue.value = ""
+    operatorValue.value = ""
+    secondValue.value = ''
+
+    result.style.display = 'none'
+})
+
+
 
