b6cc67abf1185a11b5fead1fef780d72c06b3cf8
shivsundar3009 <shivsundar3009@gmail.com>
2024-01-06 23:16:44 +0530

    something wrong with switch case

--- 03_color_mixer/script.js
+++ 03_color_mixer/script.js
@@ -2,6 +2,22 @@ const btn = document.querySelector('.btn')
 
 const resultP = document.querySelector('.resultP')
 
+
+
 btn.addEventListener('click', ()=> {
-    resultP.innerHTML = 'button working'
+
+    let color1 = document.querySelector('.color1')
+
+    let color2 = document.querySelector('.color2')
+    
+    console.log(color1.value);
+    switch (color1.value) {
+        case 'red':
+            resultP.innerHTML = 'switch casw working'
+            break;
+    
+        default:
+            break;
+    }
+    // resultP.innerHTML = 'button working'
 })
