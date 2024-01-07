1f74c3a1e9beaf6d79a33319d4d5f8350f218b1f
shivsundar3009 <shivsundar3009@gmail.com>
2024-01-07 23:23:49 +0530

    switch case working fixed error

--- 03_color_mixer/script.js
+++ 03_color_mixer/script.js
@@ -6,18 +6,62 @@ const resultP = document.querySelector('.resultP')
 
 btn.addEventListener('click', ()=> {
 
-    let color1 = document.querySelector('.color1')
+    let color1 = document.querySelector('#color1')
 
-    let color2 = document.querySelector('.color2')
-    
+    let color2 = document.querySelector('#color2')
+     
+    if(color1.value || color2.value === ""){
+        resultP.style.backgroundColor = 'red'
+        resultP.style.color = 'white'
+        resultP.style.fontWeight = '900'
+ 
+        resultP.textContent = 'please enter colors properly'
+    }
     console.log(color1.value);
     switch (color1.value) {
         case 'red':
-            resultP.innerHTML = 'switch casw working'
+            switch (color2.value) {
+                case "blue" :
+                     console.log('purple');
+                    break;
+
+                case "yellow" :
+                     console.log('orange');
+                    break;
+            
+                default:
+                    console.log('enter correct color');
+                    break;
+            }
+            break;
+
+        case 'blue':
+            switch (color2.value) {
+                case "red" :
+                     console.log('purple');
+                    break;
+            
+                case "yellow" :
+                     console.log('green');
+                    break;
+            
+                default:
+                    console.log('enter correct color');
+                    break;
+            }
             break;
     
         default:
             break;
     }
     // resultP.innerHTML = 'button working'
+})
+
+const btnClear = document.querySelector('.btnClear')
+
+btnClear.addEventListener('click',()=>{
+     color1.value = ''
+     color2.value = ''
+
+     resultP.textContent = ''
 })
