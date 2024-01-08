242b7457776f63c64637554c86edb733aa4bc84c
shivsundar3009 <shivsundar3009@gmail.com>
2024-01-08 23:36:04 +0530

    03_colorMixer assignment completed

--- 03_color_mixer/script.js
+++ 03_color_mixer/script.js
@@ -9,53 +9,66 @@ btn.addEventListener('click', ()=> {
     let color1 = document.querySelector('#color1')
 
     let color2 = document.querySelector('#color2')
+
+    console.log(color1.value);
      
-    if(color1.value || color2.value === ""){
+    if(color1.value === "" || color2.value === ""){
         resultP.style.backgroundColor = 'red'
         resultP.style.color = 'white'
         resultP.style.fontWeight = '900'
  
         resultP.textContent = 'please enter colors properly'
-    }
-    console.log(color1.value);
-    switch (color1.value) {
-        case 'red':
-            switch (color2.value) {
-                case "blue" :
-                     console.log('purple');
-                    break;
+    }else {
 
-                case "yellow" :
-                     console.log('orange');
-                    break;
-            
-                default:
-                    console.log('enter correct color');
-                    break;
-            }
-            break;
 
-        case 'blue':
-            switch (color2.value) {
-                case "red" :
-                     console.log('purple');
-                    break;
-            
-                case "yellow" :
-                     console.log('green');
-                    break;
-            
-                default:
-                    console.log('enter correct color');
-                    break;
-            }
-            break;
+        // console.log(color1.value);
+        switch (color1.value) {
+            case 'red':
+                switch (color2.value) {
+                    case "blue" :
+                        resultP.style.backgroundColor = 'purple';
+                        resultP.style.color = 'white';
+                         resultP.textContent = 'Mixed Color is : purple';
+                        break;
     
-        default:
-            break;
-    }
-    // resultP.innerHTML = 'button working'
-})
+                    case "yellow" :
+                        resultP.style.backgroundColor = 'orange';
+                        resultP.style.color = 'white';
+                        resultP.textContent = 'Mixed Color is : orange';
+                        break;
+                
+                    default:
+                        console.log('enter correct color');
+                        break;
+                }
+                break;
+    
+            case 'blue':
+                switch (color2.value) {
+                    case "red" :
+                        resultP.style.backgroundColor = 'purple';
+                        resultP.style.color = 'white';
+                        resultP.textContent = 'Mixed Color is : purple';
+                        break;
+                
+                    case "yellow" :
+                        resultP.style.backgroundColor = 'green';
+                        resultP.style.color = 'white';
+                        resultP.textContent = 'Mixed Color is : green';
+                        break;
+                
+                    default:
+                        console.log('enter correct color');
+                        break;
+                }
+                break;
+        
+            default:
+                break;
+        }
+        // resultP.innerHTML = 'button working'
+    }})
+
 
 const btnClear = document.querySelector('.btnClear')
 
